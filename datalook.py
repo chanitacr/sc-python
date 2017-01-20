9b4edff59d22fc0c2e6bb0bce0e536c1466879b2
chanitacr <chanitacr@gmail.com>
2017-01-20 12:09:11 +0000

    Update datalook.py

--- datalook.py
+++ datalook.py
@@ -8,25 +8,25 @@ import matplotlib.pyplot
 
 #filenames = sorted(glob.glob ('data/weather*.csv'))
 
-def analyse (filename, outfile=None):
-    data = numpy.loadtxt (fname=filename, delimiter=',')
+def analyse(filename, outfile=None):
+    data = numpy.loadtxt(fname=filename, delimiter=',')
     
     # Create a wide figure to hold the subplots
     fig = matplotlib.pyplot.figure (figsize=(10.0, 3.0))
     
     # Create diferrent boxis with in the main frame "place holders for plots"
-    subplot1 = fig.add_subplot (1,3,1)
-    subplot2 = fig.add_subplot (1,3,2)
-    subplot3 = fig.add_subplot (1,3,3)
+    subplot1 = fig.add_subplot(1,3,1)
+    subplot2 = fig.add_subplot(1,3,2)
+    subplot3 = fig.add_subplot(1,3,3)
 
-    subplot1.set_ylabel ('average')
-    subplot1.plot (numpy.mean (data, axis=0))
+    subplot1.set_ylabel('average')
+    subplot1.plot (numpy.mean(data, axis=0))
 
-    subplot2.set_ylabel ('minimum')
-    subplot2.plot (numpy.min (data, axis=0))
+    subplot2.set_ylabel('minimum')
+    subplot2.plot (numpy.min(data, axis=0))
 
-    subplot3.set_ylabel ('maximum')
-    subplot3.plot (numpy.max (data, axis=0))
+    subplot3.set_ylabel('maximum')
+    subplot3.plot (numpy.max(data, axis=0))
 
     #fig.tight_layout()
     if outfile is None:
@@ -42,9 +42,9 @@ def detect_problems (filename):
     sensors break.
     The funtion does not return any data.
     """
-    data = numpy.loadtxt (fname=filename, delimiter=',')
+    data = numpy.loadtxt(fname=filename, delimiter=',')
     
-    if numpy.max (data, axis = 0)[0] == 0 and numpy.max (data, axis=0)[20] == 20:
+    if numpy.max (data, axis = 0)[0] == 0 and numpy.max(data, axis=0)[20] == 20:
         print ("Suspicios looking data maxima")
     elif numpy.sum (numpy.min(data, axis=0)) == 0:
         print ("Minima add up to zero")
@@ -57,4 +57,3 @@ if __name__ == "__main__":
     print (sys.argv[1])
     analyse (sys.argv[1], outfile=sys.argv[2])
     detect_problems (sys.argv[1])
-    
